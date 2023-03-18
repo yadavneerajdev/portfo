93986d95c817ded7c1410a58a39378a12de01bb5
yadavneerajdev <connectneerajyadav@gmail.com>
2023-03-19 00:06:53 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
 
   return (
     <div className="App">
-
       <FormComponent effect={(e) => setEffect(e)} />
       <DisplayData effect={effect} />
     </div>
--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -5,6 +5,7 @@ import "./Display.css"
 
 const DisplayData = ({ effect }) => {
     const [data, setData] = useState([]);
+    const [filter, setFilter] = useState("")
 
     useEffect(() => {
         axios.get('http://localhost:5000/api/data')
@@ -16,18 +17,27 @@ const DisplayData = ({ effect }) => {
             });
     }, [effect]);
 
-    return (<div>
+    return (<div className='wrapper-cards' style={{ textAlign: "center" }}>
+        <h2>Keep Notes</h2>
+        <input type="text" placeholder='search by tag or title' value={filter} onChange={(e) => setFilter(e.target.value)} />
+        <br />
+        <span>{(filter !== "") ? `Results for  "${filter}"` : "All"}</span>
         <div className='cards'>
-            {data.map((elem, index) => {
-                return <div key={index}>
-                    <h3>{elem.title}</h3>
-                    <p className='parabody'>{elem.lbody}</p>
-                    <p className='tag'>Tags</p>
-                    <p className='labels-dis'>{elem.labels.split(" ").map((e, index) => {
-                        return <span key={index}> {e}</span>
-                    })}</p>
-                </div>
-            })}
+            {data.filter(elem => {
+                return elem.labels.split(" ").find(e => {
+                    return e.toLowerCase().includes(filter.toLowerCase())
+                }) || elem.title.toLowerCase().includes(filter.toLowerCase())
+            })
+                .map((elem, index) => {
+                    return <div key={index}>
+                        <h3>{elem.title}</h3>
+                        <p className='parabody'>{elem.lbody}</p>
+                        <p className='tag'>Tags</p>
+                        <p className='labels-dis'>{elem.labels.split(" ").map((e, index) => {
+                            return <span onClick={(e) => setFilter(e.target.innerText)} key={index}> {e}</span>
+                        })}</p>
+                    </div>
+                })}
         </div>
     </div>
     );
